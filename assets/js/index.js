5d7cad8605153cfea89cfe321394bce49a8eac23
gesora96 <nahidahmed9933@gmail.com>
2022-01-12 12:53:40 +0600

    jQuery keyboard events added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,61 @@
-// jQuery mouse events
+/* 
+
+        1.  jQuery mouse events
+        2.  jQuery keyboard events
+        3.  jQuery keyboard events
+        4.  jQuery keyboard events
+        5.  jQuery keyboard events
+        6.  jQuery keyboard events
+        7.  jQuery keyboard events
+        8.  jQuery keyboard events
+        9.  jQuery keyboard events
+        10.  jQuery keyboard events
+
+
+*/
 
 $(document).ready(() => {
+  // jQuery mouse events
+
   $('#animated').click(() => {
-    $('#animated').css('background', 'red');
-    $('#animated').css('transition', 'all 0.5s');
+    $(this).css('background', 'red');
+    $(this).css('transition', 'all 0.7s');
   });
+
   $('#animated').dblclick(() => {
-    $('#animated').css('background', 'blue');
-    $('#animated').css('transition', 'all 0.5s');
+    $(this).css('background', 'blue');
+    $(this).css('transition', 'all 0.7s');
   });
+
   $('#animated').contextmenu(() => {
-    $('#animated').css('background', 'teal');
-    $('#animated').css('transition', 'all 0.5s');
+    $(this).css('background', 'teal');
+    $(this).css('transition', 'all 0.7s');
   });
+
   $('#animated').mouseenter(() => {
-    $('#animated').css('background', 'aqua');
-    $('#animated').css('transition', 'all 0.5s');
+    $(this).css('background', 'aqua');
+    $(this).css('transition', 'all 0.7s');
   });
+
   $('#animated').mouseleave(() => {
-    $('#animated').css('background', 'aliceblue');
-    $('#animated').css('transition', 'all 0.5s');
+    $(this).css('background', 'aliceblue');
+    $(this).css('transition', 'all 0.7s');
+  });
+
+  // jQuery keyboard events
+
+  $('body').keypress(() => {
+    $('body h1').css('color', 'red');
+    $('body h1').css('transition', 'all 0.3s');
+  });
+
+  $('body').keydown(() => {
+    $('body h1').css('color', 'red');
+    $('body h1').css('transition', 'all 0.3s');
+  });
+
+  $('body').keyup(() => {
+    $('body h1').css('color', 'black');
+    $('body h1').css('transition', 'all 0.3s');
   });
 });
